fce0d907e1501993ab3d03e789670316b5d5935e
Yazid <mti.yazid@gmail.com>
2023-12-04 13:59:25 +0100

    layout

---
+++ src/components/Partners.jsx
@@ -0,0 +1,23 @@
+import Ticker from "framer-motion-ticker";
+
+function Partners() {
+  const colors = ["#632bf3", "#f122c8", "#f16022", "#9ef344", "#44d3f3"];
+  return (
+    <div className='App'>
+      <Ticker duration={20}>
+        {colors.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: item,
+              margin: "5px",
+              height: "250px",
+              width: "200px",
+            }}
+          />
+        ))}
+      </Ticker>
+    </div>
+  );
+}
+export default Partners;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "a-dark": "#14171F",
+        "a-pink": "#FFEAFD",
+        "a-blue": "#6A57E3",
+        "a-light": "#D5DEF2",
+      },
+    },
+  },
+  plugins: [],
+};
