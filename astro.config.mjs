3d646ca3a4196a531de5c15093149afb2381a084
Yazid <mti.yazid@gmail.com>
2023-12-12 14:58:16 +0100

    seo, before compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), astroImageTools, react()],
 });
